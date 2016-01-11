48a6f013865444aa732728fa317e92890be44b46
Eric van Zanten <eric.vanzanten@gmail.com>
2016-01-11 15:14:01 -0600

    Adding initial data files

---
+++ data/slugify_header.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    import sys
+    import csv
+
+    reader = csv.reader(sys.stdin)
+    
+    header = ['_'.join(r.split(' ')).lower() for r in next(reader)]
+
+    writer = csv.writer(sys.stdout)
+    writer.writerow(header)
+    writer.writerows(reader)
+    
+
