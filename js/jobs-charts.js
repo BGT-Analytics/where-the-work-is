e3b2cec246dc45400d8cf804fcff12898b2de62a
Cathy Deng <cathyd125@gmail.com>
2016-01-29 15:38:58 -0600

    flipping demand bar chart

--- js/jobs-charts.js
+++ js/jobs-charts.js
@@ -41,7 +41,7 @@ function makeDemandBarChart(element_id, data){
     var sorted_data = _.sortBy(clean_demand_data, 'demand_sum').reverse()
 
 
-    var n_cols = 16
+    var n_cols = 36
     var prepped_data = [
         {
             name: 'Higher Education',
@@ -61,7 +61,7 @@ function makeDemandBarChart(element_id, data){
 
     occupations = _.pluck(sorted_data, "occupation").slice(0,n_cols)
 
-    barHelper(element_id, prepped_data, occupations, 'Demand', 'Demand (# jobs)')
+    stackedBarHelper(element_id, prepped_data, occupations, 'Demand', 'Demand (# jobs)')
 }
 
 
@@ -224,10 +224,10 @@ function scatterHelper(element_id, prepped_data, y_label_full, y_label_short, ch
 }
 
 
-function barHelper(element_id, prepped_data, categories, y_label_full, y_label_short){
+function stackedBarHelper(element_id, prepped_data, categories, y_label_full, y_label_short){
     $(element_id).highcharts({
         chart: {
-            type: 'column'
+            type: 'bar'
         },
         credits: {
             enabled: false
@@ -270,7 +270,7 @@ function barHelper(element_id, prepped_data, categories, y_label_full, y_label_s
             layout: 'vertical',
             x: -30,
             verticalAlign: 'top',
-            y: 10,
+            y: 50,
             floating: true,
             backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
             borderColor: '#CCC',
@@ -285,7 +285,7 @@ function barHelper(element_id, prepped_data, categories, y_label_full, y_label_s
             shared: true
         },
         plotOptions: {
-            column: {
+            bar: {
                 stacking: 'normal'
             },
             series: {
@@ -308,134 +308,6 @@ function barHelper(element_id, prepped_data, categories, y_label_full, y_label_s
 
 
 
-
-
-function makeBubbleChart(data){
-    var prepped_data = []
-    $(data).each( function(i, row){
-        bubble = {
-                    x: parseFloat(row['advertised_avg_salary_entry_degree']), 
-                    y: parseFloat(row['fe_ds_ratio_log']), 
-                    z: parseFloat(row['demand_entry']), 
-                    name: shortenName(row['occupation']), 
-                    full_name: row['occupation']
-                }
-        if (!isNaN(bubble.x) && !isNaN(bubble.y) && !isNaN(bubble.z)) prepped_data.push(bubble)
-    })
-
-    $('#bubblechart').highcharts({
-
-        chart: {
-            type: 'bubble',
-            zoomType: 'xy',
-            plotBackgroundColor: {
-                linearGradient: [0, 0, 0, 450],
-                stops: [
-                    [0, 'rgb(225, 242, 241)'], //green
-                    [1, 'rgb(242, 231, 225)']  //red
-                ]
-            }
-        },
-
-        legend: {
-            enabled: false
-        },
-
-        title: {
-            //text: 'Demand & Salary by Occupation'
-            text: ''
-        },
-
-        xAxis: {
-            title: {
-                text: 'Average Salary'
-            },
-            labels: {
-                format: '£{value}'
-            },
-        },
-
-        yAxis: {
-            gridLineWidth: 0,
-            startOnTick: false,
-            endOnTick: false,
-            title: {
-                text: 'Opportunity'
-            },
-            labels: {
-                enabled: false,
-            },
-            maxPadding: 0.2
-        },
-
-        tooltip: {
-            useHTML: true,
-            headerFormat: '<table>',
-            pointFormat: '<tr><th colspan="2"><strong>{point.full_name}</strong></th></tr>' +
-                '<tr><th>Average Salary:</th><td>{point.x}</td></tr>' +
-                '<tr><th>Demand Ratio:</th><td>{point.y}</td></tr>' +
-                '<tr><th>Demand (# jobs):</th><td>{point.z}</td></tr>',
-            footerFormat: '</table>',
-            followPointer: true
-        },
-
-        plotOptions: {
-            series: {
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}',
-                    style: {
-                        fontWeight: 'normal',
-                        fontSize: '8px',
-                        color: '#333'
-                    }
-                },
-                color: '#777',
-                marker: {
-                    fillColor: '#FFFFFF',
-                    lineWidth: 1,
-                    lineColor: '#777'
-                },
-                allowPointSelect: true,
-                point: {
-                    events: {
-                        select: function () {
-                            var div = document.getElementById('occupation-detail');
-                            div.innerHTML = this.full_name+"<br/><br/>[location quotient chloropleth map here]";
-                        }
-                    }
-                }
-            }
-        },
-
-        labels: {
-            items: [
-                {
-                    html: "↑ Less Competition",
-                    style: {
-                        color: '#006167',
-                        left: '20px',
-                        top: '30px',
-                    }
-                },
-                {
-                    html: "↓ More Competition",
-                    style: {
-                        color: '#f47730',
-                        left: '20px',
-                        top: '290px',
-                    }
-                }
-            ]
-        },
-
-        series: [{
-            data: prepped_data,
-        }]
-
-    });
-}
 
 function shortenName(long_name) {
     return long_name.split(" ")[0].replace(/,/g , "");
