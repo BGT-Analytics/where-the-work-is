f393fdd1b258740842ac972db477fb60677699e1
Cathy Deng <cathyd125@gmail.com>
2016-02-01 10:17:53 -0600

    misc polish

--- js/jobs-utils.js
+++ js/jobs-utils.js
@@ -24,5 +24,12 @@ function sum(numbers) {
 
 function toTitleCase(str)
 {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return str.replace(/\w\S*/g, function(txt){
+        if (txt=='OF'||txt=='AND'||txt=='THE'){
+            return txt.toLowerCase();
+        }
+        else{
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    });
 }
