d11bd5c344e3759498fe2be096db63c08ccbfcb8
Cathy Deng <cathyd125@gmail.com>
2016-02-03 12:28:35 -0600

    occupation descriptions

--- js/jobs-mappings.js
+++ js/jobs-mappings.js
@@ -226,143 +226,143 @@ var breadcrumbs = {
 var occupation_mapping = {
     "Science, Engineering and Production Technicians":{
         "short_name":"Engineering Techs",
-        "description":"",
+        "description":"Science, engineering and production technicians perform a variety of technical support functions to assist the work of scientists and technologists, assist in the design, development, production and maintenance of electronic systems, perform technical quality assurance related tasks, support the work of building and civil engineers, and perform various other technical support functions for engineers.",
         "example_titles":""},
     "Draughtspersons and Related Architectural Technicians":{
         "short_name":"Architectural Techs",
-        "description":"",
+        "description":"Workers in this group prepare technical drawings, plans and charts and give technical assistance to planners and architects.",
         "example_titles":""},
     "Information Technology Technicians":{
         "short_name":"IT Techs",
-        "description":"",
+        "description":"Workers in this group are responsible for the day-to-day running of IT systems and networks, and provide technical support, advice and guidance for users and customers.",
         "example_titles":""},
     "Health Associate Professionals":{
         "short_name":"Health Assoc. Profnls",
-        "description":"",
+        "description":"Workers in this group provide a variety of technical support functions for a range of health professionals such as administering first aid treatment in emergencies, operating x-ray and other imaging equipment, fitting hearing aids and spectacles, dispensing medicines, and fitting, servicing and repairing medical and dental devices.",
         "example_titles":""},
     "Welfare and Housing Associate Professionals":{
         "short_name":"Welfare Assoc. Profnls",
-        "description":"",
+        "description":"Welfare and housing associate professionals organise and provide social welfare and related community services including working with children and young people, address the housing needs of individuals or localities, assist those with physical and mental disabilities or illnesses, investigate cases of abuse or neglect, provide counselling services and perform other welfare functions.",
         "example_titles":""},
     "Protective Service Occupations":{
         "short_name":"Protective Service",
-        "description":"",
+        "description":"Workers in this group serve in Her Majesty’s, foreign and Commonwealth armed forces, investigate crimes and maintain law and order, fight fires and advise on fire prevention, guard inmates and maintain discipline at prisons and detention centres, and perform other miscellaneous protective service roles.",
         "example_titles":""},
     "Artistic, Literary and Media Occupations":{
         "short_name":"Artistic/Media Occs",
-        "description":"",
+        "description":"Workers in this group create and restore artistic work; write, evaluate and edit literary material; translate written and spoken statements; perform in films, theatre and other acts of entertainment; write, arrange and perform musical compositions; produce, direct, present and participate in television programmes, films and stage presentations; create artistic content for various media; and promote and administer artistic and cultural activities.",
         "example_titles":""},
     "Design Occupations":{
         "short_name":"Design Occs",
-        "description":"",
+        "description":"Workers in this group use a wide variety of design techniques to convey information for publicity, promotional and advertising purposes, and to design industrial and commercial products, clothing and fashion accessories.",
         "example_titles":""},
     "Sports and Fitness Occupations":{
         "short_name":"Sports Occs",
-        "description":"",
+        "description":"Workers in this group prepare for and compete in sporting events for financial gain, train amateur and professional sportsmen and women to enhance performance, promote participation and standards in sport, organise and officiate at sporting events, and provide instruction, training and supervision for various forms of exercise and other recreational activities.",
         "example_titles":""},
     "Transport Associate Professionals":{
         "short_name":"Transport Assoc. Profnls",
-        "description":"",
+        "description":"Workers in this group command and navigate aircraft and vessels, perform technical functions to operate and maintain such craft, and plan and regulate the ground and air movements of aircraft.",
         "example_titles":""},
     "Legal Associate Professionals":{
         "short_name":"Legal Assoc. Profnls",
-        "description":"",
+        "description":"Legal associate professionals organise the administrative work of legal practices and perform specialised legal duties.",
         "example_titles":""},
     "Business, Finance and Related Associate Professionals":{
         "short_name":"Business Assoc. Profnls",
-        "description":"",
+        "description":"Business, finance and related associate professionals calculate the probable costs of projects, assess the value of properties, underwrite insurance policies and assess liability regarding claims, deal in commodities and financial assets, advise clients on insurance, investment, taxation, mortgages, pensions and other financial matters, and assist accounting and financial professionals in managing an organisation’s financial affairs and its accounts.",
         "example_titles":""},
     "Sales, Marketing and Related Associate Professionals":{
         "short_name":"Sales  Assoc. Profnls",
-        "description":"",
+        "description":"Sales, marketing and related associate professionals purchase raw materials, equipment and merchandise, provide technical sales advice to customers, undertake market research, support the implementation of the organisation’s marketing and sales policies and arrange for the trading and leasing of property on behalf of clients.",
         "example_titles":""},
     "Conservation and Environmental Associate Professionals":{
         "short_name":"Environmental Assoc. Profnls",
-        "description":"",
+        "description":"Workers in this group are responsible for the day-to-day planning, management, promotion and maintenance of areas of the environment that are of benefit to wildlife and the public.",
         "example_titles":""},
     "Public Services and Other Associate Professionals":{
         "short_name":"Public Services Assoc. Profnls",
-        "description":"",
+        "description":"Public services and other associate professionals supervise, manage and undertake general administrative functions in national and local government, advise upon and undertake recruitment, staff appraisal and industrial relations activities, give advice regarding careers, training and related opportunities, provide vocational training, undertake inspections and investigations to ensure statutory compliance, implement health and safety measures within establishments and organisations, and undertake inspections to ensure compliance with environmental health regulations.",
         "example_titles":""},
     "Administrative Occupations: Finance":{
         "short_name":"Administrative: Finance",
-        "description":"",
+        "description":"Workers in this group perform administrative and other tasks in relation to credit control and debt collection, the maintenance of financial records within firms, financial transactions made with customers and the collection of payments from businesses and households.",
         "example_titles":""},
     "Administrative Occupations: Records":{
         "short_name":"Administrative: Records",
-        "description":"",
+        "description":"Workers in this group create, maintain, update and file correspondence, data, documents and information held both in hard copy and electronically for storage, reference purposes and despatch.",
         "example_titles":""},
     "Administrative Occupations: Office Managers and Supervisors":{
         "short_name":"Administrative: Office Mngrs",
-        "description":"",
+        "description":"Workers in this group coordinate the day-to-day running of offices providing the administrative services of commercial, industrial and other non-governmental organisations and public agencies, and supervise the staff within those offices.",
         "example_titles":""},
     "Secretarial and Related Occupations":{
         "short_name":"Secretarial Occs",
-        "description":"",
+        "description":"Workers in this group provide dictation services, type, edit and print documents, perform general clerical and organisational duties in support of management or other workers, and receive and direct clients and visitors to commercial, government and other establishments.",
         "example_titles":""},
     "Agricultural and Related Trades":{
         "short_name":"Agricultural Trades",
-        "description":"",
+        "description":"Those working in agricultural and related trades occupations cultivate crops and raise animals for consumption, grow plants, trees, shrubs and flowers for sale, tend private and public gardens, parks, sports pitches and other recreational areas, and perform a variety of other skilled occupations related to agriculture and fishing.",
         "example_titles":""},
     "Metal Forming, Welding and Related Trades":{
         "short_name":"Metal Forming Trades",
-        "description":"",
+        "description":"Metal forming, welding and related trades workers shape, cast, finish and join metal, and erect, install, maintain and repair metal structures and fixtures.",
         "example_titles":""},
     "Metal Machining, Fitting and Instrument Making Trades":{
         "short_name":"Metal Machining Trades",
-        "description":"",
+        "description":"Metal machining, fitting and instrument making trades workers mark out metal for machine tool working, set up and operate lathes, boring, drilling, grinding, milling machines and presses, assemble and repair machine tools, install and repair plant and industrial machinery, fix and assemble parts and sub-assemblies in the manufacture of metal products, make, calibrate, test and repair precision and optical instruments, and install and repair air-conditioning and refrigeration systems.",
         "example_titles":""},
     "Vehicle Trades":{
         "short_name":"Vehicle Trades",
-        "description":"",
+        "description":"Vehicle trades workers repair, service and maintain the bodies, engines, parts, sub-assemblies, internal trimmings, upholstery and exterior surfaces of vehicles.",
         "example_titles":""},
     "Electrical and Electronic Trades":{
         "short_name":"Electrical/Electronic Trades",
-        "description":"",
+        "description":"Workers in electrical and electronic trades install wiring in road and rail vehicles and aircraft and assemble, install, maintain, test and repair electrical and electronic equipment, components and systems concerned with lighting, signalling, telecommunications, radio and television and other commercial, industrial and domestic functions.",
         "example_titles":""},
     "Skilled Metal, Electrical and Electronic Trades Supervisors":{
         "short_name":"Metal Trades Supvrs",
-        "description":"",
+        "description":"Jobholders in this group supervise and control technical and operational aspects of metal forming, welding and relate, metal machining, fitting and instrument making, vehicles trades and electrical and electronic trades.",
         "example_titles":""},
     "Construction and Building Trades":{
         "short_name":"Construction Trades",
-        "description":"",
+        "description":"Jobholders within construction and building trades erect and fit metal framework for building construction, cut, shape and lay stone, brick and similar materials, cover roofs and exterior walls, install, maintain and repair plumbing, heating and ventilating systems, construct and install wooden frameworks and fittings, fi glass into windows and doors, and perform other miscellaneous construction tasks.",
         "example_titles":""},
     "Building Finishing Trades":{
         "short_name":"Building Finishing Trades",
-        "description":"",
+        "description":"Workers in this group apply plaster and cement mixtures to walls and ceilings, lay flooring covers and apply paint, varnish, wallpaper, tiles and other protective and decorative materials to walls and ceilings.",
         "example_titles":""},
     "Construction and Building Trades Supervisors":{
         "short_name":"Construction Trades Supvrs",
-        "description":"",
+        "description":"Workers in this group supervise and control technical and operational aspects of Construction and Building, and Building Finishing Trades.",
         "example_titles":""},
     "Textiles and Garments Trades":{
         "short_name":"Textiles Trades",
-        "description":"",
+        "description":"Workers within textiles and garments trades weave fabrics into fire and carpet, knit garments from yarn, upholster the seating and interior of vehicles and planes, make soft furnishings, make, repair and finish leather goods, and make, fi and alter tailored articles of clothing.",
         "example_titles":""},
     "Printing Trades":{
         "short_name":"Printing Trades",
-        "description":"",
+        "description":"Printing trades workers compose and set type and printing blocks, produce printing plates, cylinders and film, operate printing machines and bind the finished printed product.",
         "example_titles":""},
     "Food Preparation and Hospitality Trades":{
         "short_name":"Hospitality Trades",
-        "description":"",
+        "description":"Workers in food preparation and hospitality trades slaughter livestock, cut, trim and prepare meat, poultry and fish, prepare, bake and finish bread and flour confectionery products, prepare food in hotels, restaurants and other establishments, and manage the catering and bar facilities in factories, shops, theatres, educational premises and similar establishments.",
         "example_titles":""},
     "Other Skilled Trades":{
         "short_name":"Skilled Trades (Other)",
-        "description":"",
+        "description":"Workers in this unit group perform a variety of other skilled craft and related trades.",
         "example_titles":""},
     "Childcare and Related Personal Services":{
         "short_name":"Childcare",
-        "description":"",
+        "description":"Workers in childcare and related personal services supervise play and other activities for pre-school age children, assist teachers and care for children in day or residential nurseries, children’s homes and private households.",
         "example_titles":""},
     "Animal Care and Control Services":{
         "short_name":"Animal Care",
-        "description":"",
+        "description":"Workers in this group provide assistance to veterinarians, carry out pest control services, care for animals in stables, kennels, zoos and other such establishments, provide specialised grooming and clipping services for animals and capture stray or unruly dogs.",
         "example_titles":""},
     "Caring Personal Services":{
         "short_name":"Personal Care",
-        "description":"",
+        "description":"Those working in caring personal services occupations transport patients by ambulance, stretcher, wheelchair or other means and assist health professionals with the care of patients in hospitals, dental surgeries, nursing homes, residential homes, clinics, day care services and within the home.",
         "example_titles":""}
 }
 
