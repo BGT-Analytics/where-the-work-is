3a60bd81d319ec5c159b2c9ec7404198a1ef2deb
Derek Eder <derek.eder@gmail.com>
2016-02-15 09:47:20 -0600

    added LEP locations

---
+++ data/lep_locations.js
@@ -0,0 +1,128 @@
+var lep_locations = [
+  {
+    "location_label": "Greater Birmingham",
+    "lep": "Greater Birmingham and Solihull",
+    "lat": 52.483056,
+    "lon": -1.893611
+  },
+  {
+    "location_label": "Bristol Region",
+    "lep": "West of England",
+    "lat": 51.45,
+    "lon": -2.583333
+  },
+  {
+    "location_label": "Greater Manchester",
+    "lep": "Greater Manchester",
+    "lat": 53.466667,
+    "lon": -2.233333
+  },
+  {
+    "location_label": "Leeds City Region",
+    "lep": "Leeds City Region",
+    "lat": 53.799722,
+    "lon": -1.549167
+  },
+  {
+    "location_label": "Liverpool City Region",
+    "lep": "Liverpool City Region",
+    "lat": 53.4,
+    "lon": -3
+  },
+  {
+    "location_label": "Nottingham City Region",
+    "lep": "Derby, Derbyshire, Nottingham and Nottinghamshire",
+    "lat": 52.95,
+    "lon": -1.133333
+  },
+  {
+    "location_label": "Newcastle Region",
+    "lep": "North Eastern",
+    "lat": 54.966667,
+    "lon": -1.6
+  },
+  {
+    "location_label": "Sheffield City Region",
+    "lep": "Sheffield City Region",
+    "lat": 53.383611,
+    "lon": -1.466944
+  },
+  {
+    "location_label": "Greater Brighton",
+    "lep": "Coast to Capital",
+    "lat": 50.842941,
+    "lon": -0.131312
+  },
+  {
+    "location_label": "Coventry and Warwickshire",
+    "lep": "Coventry and Warwickshire",
+    "lat": 52.408056,
+    "lon": -1.510556
+  },
+  {
+    "location_label": "Hull and the Humber",
+    "lep": "Humber",
+    "lat": 53.75,
+    "lon": -0.333333
+  },
+  {
+    "location_label": "Great Ipswich & Greater Norwich",
+    "lep": "New Anglia",
+    "lat": 52.41,
+    "lon": 0.74
+  },
+  {
+    "location_label": "Leicester and Leicestershire",
+    "lep": "Leicester and Leicestershire",
+    "lat": 52.633333,
+    "lon": -1.133333
+  },
+  {
+    "location_label": "Oxford and Oxfordshire",
+    "lep": "Oxfordshire",
+    "lat": 51.751944,
+    "lon": -1.257778
+  },
+  {
+    "location_label": "Plymouth and South West Peninsula",
+    "lep": "Heart of the South West",
+    "lat": 50.903,
+    "lon": -3.488
+  },
+  {
+    "location_label": "Plymouth and South West Peninsula",
+    "lep": "Cornwall and Isles of Scilly",
+    "lat": 50.903,
+    "lon": -3.488
+  },
+  {
+    "location_label": "Preston, South Ribble and Lancashire",
+    "lep": "Lancashire",
+    "lat": 53.759,
+    "lon": -2.699
+  },
+  {
+    "location_label": "Southampton and Portsmouth",
+    "lep": "Solent",
+    "lat": 50.8542,
+    "lon": -1.1755
+  },
+  {
+    "location_label": "Southend-on-Sea",
+    "lep": "South East",
+    "lat": 51.340203,
+    "lon": 0.730922
+  },
+  {
+    "location_label": "Stoke-on-Trent and Staffordshire",
+    "lep": "Stoke-on-Trent and Staffordshire",
+    "lat": 53,
+    "lon": -2.183333
+  },
+  {
+    "location_label": "Swindon and Wiltshire",
+    "lep": "Swindon and Wiltshire",
+    "lat": 51.56,
+    "lon": -1.78
+  }
+]
